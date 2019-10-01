92ab02944176428d934878a7074089173e67f55e
Christian Haney <christianhaney1996@gmail.com>
2019-10-01 16:26:41 -0700

    JS | Matching Game - Christian Haney functionality added functionality to determine if two cards match or not matched cards now hide themselves with visibility: hidden cards that do not match are flipped back over after 1.5 seconds refactored to make card flipping and hiding independent functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,47 @@ function initializeApp() {
   $(".game-area").on("click", ".card", handleCardClick)
 }
 
+var firstClicked = null;
+var secondClicked = null;
+var matches = null;
+
 function handleCardClick(event) {
   var targetCard = $(event.currentTarget);
-  targetCard.toggleClass("flipped")
+  flipCard(targetCard)
+  if (!firstClicked) {
+    firstClicked = targetCard;
+    return;
+  } else {
+    secondClicked = targetCard;
+  }
+  var firstImage = firstClicked.find(".front").css("background-image")
+  var secondImage = secondClicked.find(".front").css("background-image")
+  if (firstImage === secondImage) {
+    console.log("Cards Match")
+    matches += 1;
+    hideCard(firstClicked, secondClicked)
+    firstClicked = null;
+    secondClicked = null;
+  } else {
+    $(".game-area").unbind("click")
+    console.log("Try Again")
+    setTimeout(function(){
+      flipCard(firstClicked, secondClicked)
+      firstClicked = null;
+      secondClicked = null;
+      $(".game-area").on("click", ".card", handleCardClick)
+    }, 1500)
+  }
+}
+
+function flipCard() {
+  for (var card in arguments) {
+    arguments[card].toggleClass("flipped")
+  }
+}
+
+function hideCard() {
+  for (var card in arguments) {
+    arguments[card].css("visibility", "hidden")
+  }
 }
