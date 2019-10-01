c145f0b46b2b1a49a0d44ce8b0aec28b30478fb5
Christian Haney <christianhaney1996@gmail.com>
2019-10-01 15:41:03 -0700

    JS | Matching Game - Christian Haney functionality and styling added background images to the front and back divs of the cards added the ability to flip cards on click animated the transition from face down to flipped

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+$(document).ready(initializeApp)
 
+function initializeApp() {
+  $(".game-area").on("click", ".card", handleCardClick)
+}
+
+function handleCardClick(event) {
+  var targetCard = $(event.currentTarget);
+  targetCard.toggleClass("flipped")
+}
