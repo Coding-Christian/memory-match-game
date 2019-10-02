1565efb95ba6c1ff2bd820d9950fabda523ff7d5
Christian Haney <christianhaney1996@gmail.com>
2019-10-02 16:57:20 -0700

    JS | Matching Game - Christian Haney stat functionality added stat tracking functionality minor refactoring on HTML CSS and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
-$(document).ready(initializeApp)
+$(document).ready(initializeApp);
 
 function initializeApp() {
-  $(".game-area").on("click", ".card", handleCardClick)
-  // $(".reset-button").click(resetGame)
+  $(".game-area").on("click", ".card", handleCardClick);
+  // $(".reset-button").click(resetGame);
+  updateStats();
 }
 
 var firstClicked = null;
 var secondClicked = null;
-var matches = null;
+var matches = 0;
 var maxMatches = 9;
+var attempts = 0;
+var gamesPlayed = 0;
 
 function handleCardClick(event) {
   var targetCard = $(event.currentTarget);
   if (!firstClicked) {
     firstClicked = targetCard;
-    flipCard(targetCard)
+    flipCard(targetCard);
     return;
   } else {
     secondClicked = targetCard;
@@ -23,47 +26,61 @@ function handleCardClick(event) {
     secondClicked = null;
     return;
   }
-  flipCard(targetCard)
-  checkMatch(firstClicked, secondClicked)
+  flipCard(targetCard);
+  attempts++;
+  checkMatch(firstClicked, secondClicked);
+  updateStats();
 }
 
 function checkMatch(card1, card2) {
-  var firstImage = card1.find(".front").css("background-image")
-  var secondImage = card2.find(".front").css("background-image")
+  var firstImage = card1.find(".front").css("background-image");
+  var secondImage = card2.find(".front").css("background-image");
   if (firstImage === secondImage) {
-    console.log("Cards Match")
-    matches += 1;
-    hideCard(firstClicked, secondClicked)
+    console.log("Cards Match");
+    matches++;
+    hideCard(firstClicked, secondClicked);
     firstClicked = null;
     secondClicked = null;
-    checkWin()
+    checkWin();
   } else {
-    $(".game-area").unbind("click")
-    console.log("Try Again")
+    $(".game-area").unbind("click");
+    console.log("Try Again");
     setTimeout(function () {
-      flipCard(firstClicked, secondClicked)
+      flipCard(firstClicked, secondClicked);
       firstClicked = null;
       secondClicked = null;
-      $(".game-area").on("click", ".card", handleCardClick)
-    }, 1500)
+      $(".game-area").on("click", ".card", handleCardClick);
+    }, 1500);
   }
 }
 
 function checkWin() {
   if (matches === maxMatches) {
-    console.log("You Win")
-    $(".gameover-modal").removeClass("hidden")
+    console.log("You Win");
+    setTimeout(function () { $(".gameover-modal").removeClass("hidden");}, 1000)
+    gamesPlayed++
+  }
+}
+
+function updateStats() {
+  $("#gamesPlayed").text(gamesPlayed);
+  $("#matchAttempts").text(attempts);
+  var accuracy = (matches / attempts * 100).toFixed(2);
+  if (accuracy === "NaN") {
+    $("#accuracy").text("0.00%");
+  } else {
+    $("#accuracy").text(accuracy + "%");
   }
 }
 
 function flipCard() {
   for (var card in arguments) {
-    arguments[card].toggleClass("flipped")
+    arguments[card].toggleClass("flipped");
   }
 }
 
 function hideCard() {
   for (var card in arguments) {
-    arguments[card].toggleClass("hidden")
+    arguments[card].toggleClass("hidden");
   }
 }
